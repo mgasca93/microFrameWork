884820b83be5959e6596c55adb4b78f8b4a64b40
Mario Gasca <web@mariogasca.com>
2021-03-05 13:03:56 -0600

    Se renombraron archivos base y se agrego al controlador base la funcion render global

--- controllers/welcome/welcomeController.php
+++ controllers/welcome/welcomeController.php
@@ -4,9 +4,5 @@ class Welcome extends Controller{
 
     public function __construct(){
         parent::__construct();        
-    } 
-          
-    public function render($view){
-        $this->view->render($view);
-    }    
+    }             
 }
--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 //Cargamos el core de la app
 require_once "app/app.php";
 require_once "app/session/session.php";
-require_once "app/Appmodel.php";
-require_once "app/Appcontroller.php";
-require_once "app/Appview.php";
+require_once "app/model.php";
+require_once "app/controller.php";
+require_once "app/view.php";
 
 //Cargamos la configuracion de la app
 require_once "config/config.php";
