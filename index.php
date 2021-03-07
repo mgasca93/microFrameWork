5a5affa4deea698ae26ead0326c8fc342349f83f
Mario Gasca <web@mariogasca.com>
2021-03-07 14:23:31 -0600

    Se creo la clase principal microFramework donde se gestiona la auto carga de clases, el modo debug para la aplicacion y el objeto de la clase app

--- index.php
+++ index.php
@@ -1,16 +1,65 @@
 <?php
 
-//Cargamos el core de la app
-require_once "app/app.php";
-require_once "app/session/session.php";
-require_once "app/model.php";
-require_once "app/controller.php";
-require_once "app/view.php";
+class microFramework{
 
-//Cargamos la configuracion de la app
-require_once "config/config.php";
-require_once "config/database.php";
+    public $directory;
 
+    public function __construct($flagDebug = false){
+    
+        $this->debug($flagDebug);
+        /**
+         * Establecemos los directorios de carga
+         */
+        $this->directory = array(
+            'app',
+            'config',
+        );
 
-$app = new App();
-$app->run();
+        /**
+         * Creamos la auto carga de clases
+         */
+        for($i = 0; $i < count($this->directory); $i++){
+            $this->autoload($this->directory[$i]);
+        }        
+
+        /**
+         * Se inicializa la aplicacion
+         */
+        $app = new App();
+        $app->run();
+    }
+
+    /**
+     * Me permite gestionar el modo debug para la
+     */
+    public function debug($flag){
+        if($flag){
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+        }
+    }
+
+    public function autoload($directorio)
+    {
+        //Obtengo todos los archivos dentro del directorio dado
+        $archivos = array_diff(scandir($directorio), array('.','..'));
+        foreach($archivos as $archivo)
+        {
+            //Verifico si es un subdirectorio y lo cargo de forma recursiva
+            $ruta = $directorio . "/" . $archivo;
+            if(is_dir($ruta))
+            {
+                $this->autoload($ruta);
+            }
+
+            //Si es un archivo creo la auto carga
+            if(file_exists($ruta) && is_file($ruta))
+            {
+                require_once $ruta;                
+            }             
+        }
+    }
+}
+
+$aplication = new microFramework();
