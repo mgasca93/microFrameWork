9d2006c3f5a99622d81f46d21665d08c4a66caf8
Mario Gasca <web@mariogasca.com>
2021-03-07 13:07:12 -0600

    Se establecieron las constantes base

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define("URL","YOUR DOMAIN");
-define("HOST","YOUR HOST");
-define("DBNAME","YOUR DBNAME");
-define("USERNAME","YOUR USERNAME");
-define("PASSWORD","YOUR PASSWORD DB");
-define("CHARSET","YOUR DB CODING");
+define("URL","DOMAIN");
+define("HOST","HOST");
+define("DBNAME","DBNAME");
+define("USERNAME","USERNAME");
+define("PASSWORD","PASSWORD");
+define("CHARSET","DBENCODING");
