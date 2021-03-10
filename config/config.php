bd4cc51a2c46371ae4b3d1063effdd6f63074cac
Mario Gasca <web@mariogasca.com>
2021-03-10 13:06:27 -0600

    Se añadio el timezone por defecto en el proyecto

--- config/config.php
+++ config/config.php
@@ -1,8 +1,9 @@
 <?php
 
-define("URL","DOMAIN");
-define("HOST","HOST");
-define("DBNAME","DBNAME");
-define("USERNAME","USERNAME");
-define("PASSWORD","PASSWORD");
-define("CHARSET","DBENCODING");
+define("URL","http://microframework.test/");
+define("HOST","localhost");
+define("DBNAME","default");
+define("USERNAME","root");
+define("PASSWORD","");
+define("CHARSET","utf8mb4");
+date_default_timezone_set('America/Mexico_City');
