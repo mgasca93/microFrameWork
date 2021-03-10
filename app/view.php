18d2da95ddd81028fa4fcf54f8826793236be781
Mario Gasca <web@mariogasca.com>
2021-03-09 18:45:17 -0600

    Se agrego el metodo que se encargado de hashear el nombre de la view

--- app/view.php
+++ app/view.php
@@ -1,8 +1,10 @@
 <?php 
 class View{
 
-    public function __construct(){
-       
+    public $pageTitle;
+    
+    public function __construct($page){
+       $this->pageTitle = $this->setTitle($page);
     }
 
     public function render($view){
@@ -10,5 +12,33 @@ class View{
         if(file_exists($archivoView)){
             require_once $archivoView;
         }
+    }
+
+    /**
+     * Al utilizar el nombre del controlador como titulo de la pagina
+     * es posible recibir por ej. WelcomeControllerDefault y es necesario
+     * añadir espacios entre cada palabra
+     */
+    public function setTitle($titlePage)
+    {        
+        $pageTitleArray = str_split($titlePage);
+        $title = '';
+        $counter = 0;
+        foreach($pageTitleArray as $letter)
+        {            
+            if(!ctype_upper($letter) && $counter == 0)
+            {
+                $title .= $letter;
+            }else if(ctype_upper($letter) && $counter > 0)
+            {
+                $title .= ' ' . $letter;
+            }else
+            {
+                $title .= $letter;
+            }
+            $counter++;
+        }          
+        $title = ucfirst($title);
+        return $title;
     }
 }
