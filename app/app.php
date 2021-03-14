63caef7824959c6baa32c25544a40abb199d7018
Mario Gasca Lopez <web@mariogasca.com>
2021-03-14 14:38:35 -0600

    Se añadieron bloques try catch en el core de la aplicación para el manejo de excepciones en tiempo de ejecucion

--- app/app.php
+++ app/app.php
@@ -17,9 +17,9 @@ class App{
      * Punto de entrada de la aplicacion
      */
     final public function run(){
-         
+                 
         $archivoController = "controllers/" . $this->url[0] . "/" . $this->url[0] . "Controller.php";        
-        
+    
         if(file_exists($archivoController)){
             require_once $archivoController;
             $this->controller = new $this->url[0]($this->url[0]);        
@@ -33,17 +33,27 @@ class App{
                     for($i = 2; $i < $nparams; $i++){
                         array_push($params, $this->url[$i]);
                     }
-                    $this->controller->{$this->url[1]}($params);
-                }else{
-                    $this->controller->{$this->url[1]}();
+                    try{
+                        $this->controller->{$this->url[1]}($params);
+                    }
+                    catch(Throwable $t){
+                        $this->controller->getException($t->getMessage());                        
+                    }
+                }else{    
+                    try{
+                        $this->controller->{$this->url[1]}();                                 
+                    }
+                    catch(Throwable $t){     
+                        $this->controller->getException($t->getMessage());                     
+                    }               
                 }                
             }else{
-                $this->controller->render($this->url[0]);
+                $this->controller->index($this->url[0]);
             }
         }else{
             require_once "controllers/404/errorController.php";
             $this->controller = new ManagerError('404 Page not found');
-            $this->controller->render("404");
-        }
+            $this->controller->index("404");
+        }               
     }
 }
